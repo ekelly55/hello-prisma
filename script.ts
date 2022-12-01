d98da6bf3421ef14e817247b92bc191042069a44
ekelly55 <eamonn.michael.kellyr@email.com>
2022-12-01 18:29:34 -0500

    added users and a post, retrieved users and a post

--- script.ts
+++ script.ts
@@ -4,8 +4,12 @@ const prisma = new PrismaClient()
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  const users = await prisma.user.findMany()
-  console.log(users)
+  const usersWithPosts = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  })
+  console.dir(usersWithPosts, { depth: null })
 }
 
 main()
